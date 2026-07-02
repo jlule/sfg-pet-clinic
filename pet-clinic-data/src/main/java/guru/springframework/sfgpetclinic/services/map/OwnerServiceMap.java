5362ec4ca1ed6a156b34cb0aca2a13618951f5f3
James Lule <43647523+jlule@users.noreply.github.com>
2026-07-01 23:09:55 -0600

    Adding Service Config. Closes issue  #22

--- pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/services/map/OwnerServiceMap.java
+++ pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/services/map/OwnerServiceMap.java
@@ -6,7 +6,7 @@ import guru.springframework.sfgpetclinic.services.OwnerService;
 import org.springframework.stereotype.Service;
 
 import java.util.Set;
-//@Service
+@Service
 public class OwnerServiceMap extends AbstractMapService<Owner,Long> implements OwnerService{
 
     @Override
