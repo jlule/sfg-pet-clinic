e42b781dc85e7215cc987450857959f17eedc70a
James Lule <43647523+jlule@users.noreply.github.com>
2026-07-03 03:02:24 -0600

    ! Refactored Map impl to abstract ID  generation closes   issue #2 basically automats the ID creation in the save method

--- pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/services/map/PetServiceMap.java
+++ pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/services/map/PetServiceMap.java
@@ -20,7 +20,8 @@ public class PetServiceMap extends AbstractMapService<Pet, Long> implements Crud
 
     @ Override
     public Pet save(Pet object) {
-        return super.save(object.getId(), object);
+
+        return super.save(object);
     }
 
     @Override
